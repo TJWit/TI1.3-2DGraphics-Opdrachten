05897c000bed09c6c6c410f440a541f38658e475
Thom de Wit <kersjan14.de.wit@gmail.com>
2023-01-30 14:45:00 +0100

    opdracht 1
    probeerde met een lijst van coörds maar lukte niet

--- Week1/001.House/src/House.java
+++ Week1/001.House/src/House.java
@@ -5,11 +5,15 @@ import javafx.scene.canvas.Canvas;
 import javafx.stage.Stage;
 import org.jfree.fx.FXGraphics2D;
 
+import java.awt.geom.Line2D;
+import java.util.ArrayList;
+
 public class House extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Canvas canvas = new Canvas(1920, 1080);
         draw(new FXGraphics2D(canvas.getGraphicsContext2D()));
+
         primaryStage.setScene(new Scene(new Group(canvas)));
         primaryStage.setTitle("House");
         primaryStage.show();
@@ -17,6 +21,18 @@ public class House extends Application {
 
 
     public void draw(FXGraphics2D graphics) {
+        graphics.draw(new Line2D.Double(100, 920, 800, 920));
+        graphics.draw(new Line2D.Double(800, 920, 800, 480));
+        graphics.draw(new Line2D.Double(800, 480, 450, 80));
+        graphics.draw(new Line2D.Double(450, 80, 100, 480));
+        graphics.draw(new Line2D.Double(100, 480, 100, 920));
+        graphics.draw(new Line2D.Double(150, 920, 150, 620));
+        graphics.draw(new Line2D.Double(150, 620, 300, 620));
+        graphics.draw(new Line2D.Double(300, 620, 300, 920));
+        graphics.draw(new Line2D.Double(350, 780, 750, 780));
+        graphics.draw(new Line2D.Double(750, 780, 750, 620));
+        graphics.draw(new Line2D.Double(750, 620, 350, 620));
+        graphics.draw(new Line2D.Double(350, 620, 350, 780));
     }
 
 
@@ -24,5 +40,7 @@ public class House extends Application {
     public static void main(String[] args) {
         launch(House.class);
     }
+
+
 
 }
