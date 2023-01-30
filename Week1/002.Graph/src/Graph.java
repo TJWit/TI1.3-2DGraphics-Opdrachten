8bba86ab47f58543344cbaaa61fd4b01f5a4bc84
Thom de Wit <kersjan14.de.wit@gmail.com>
2023-01-30 15:03:47 +0100

    opdracht 2

--- Week1/002.Graph/src/Graph.java
+++ Week1/002.Graph/src/Graph.java
@@ -9,9 +9,10 @@ import javafx.stage.Stage;
 import org.jfree.fx.FXGraphics2D;
 
 public class Graph extends Application {
+    private Canvas canvas;
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Canvas canvas = new Canvas(1920, 1080);
+        this.canvas = new Canvas(1920, 1080);
         draw(new FXGraphics2D(canvas.getGraphicsContext2D()));
         primaryStage.setScene(new Scene(new Group(canvas)));
         primaryStage.setTitle("Graph");
@@ -20,6 +21,19 @@ public class Graph extends Application {
     
     
     public void draw(FXGraphics2D graphics) {
+        double prevY = (1080-100);
+        double prevX = 50;
+        double y;
+
+        graphics.draw(new Line2D.Double(50,980, 1920, 980));
+        graphics.draw(new Line2D.Double(50, 980, 50, 0));
+
+        for (double x = 52; x < 1820; x+=2) {
+            y = 980 - (Math.pow(((x-50)/30), 3));
+            graphics.draw(new Line2D.Double(prevX, prevY, x, y));
+            prevX = x;
+            prevY = y;
+        }
     }
     
     
