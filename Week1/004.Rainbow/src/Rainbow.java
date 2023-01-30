1598672055837183899bd717f4f4eac60e7d0deb
Thom de Wit <kersjan14.de.wit@gmail.com>
2023-01-30 16:31:00 +0100

    opdracht 4

--- Week1/004.Rainbow/src/Rainbow.java
+++ Week1/004.Rainbow/src/Rainbow.java
@@ -9,9 +9,10 @@ import javafx.stage.Stage;
 import org.jfree.fx.FXGraphics2D;
 
 public class Rainbow extends Application {
+    private Canvas canvas;
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Canvas canvas = new Canvas(1920, 1080);
+        this.canvas = new Canvas(1920, 1080);
         draw(new FXGraphics2D(canvas.getGraphicsContext2D()));
         primaryStage.setScene(new Scene(new Group(canvas)));
         primaryStage.setTitle("Rainbow");
@@ -20,6 +21,23 @@ public class Rainbow extends Application {
     
     
     public void draw(FXGraphics2D graphics) {
+        graphics.translate(this.canvas.getWidth()/2, this.canvas.getHeight()/2);
+
+        double r1 = 300;
+        double r2 = 200;
+
+        graphics.draw(new Line2D.Double(-500, 0, 500, 0));
+        graphics.draw(new Line2D.Double(0, -500, 0, 500));
+
+        for (double i = 0; i <= 2*Math.PI; i+=1/500f) {
+            double x1 = r1 * Math.cos(i);
+            double y1 = r1 * Math.sin(i);
+            double x2 = r2 * Math.cos(i);
+            double y2 = r2 * Math.sin(i);
+
+            graphics.setColor(Color.getHSBColor((float)(i/2/Math.PI), 1, 1));
+            graphics.draw(new Line2D.Double(x1, y1, x2, y2));
+        }
 
     }
     
