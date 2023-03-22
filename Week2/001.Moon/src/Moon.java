44aa46d3a2646fcf1481640b7bcfaa46eb934a3c
kersjan14 <kersjan14.de.wit@gmail.com>
2023-03-22 15:16:27 +0100

    2.1 ~

--- Week2/001.Moon/src/Moon.java
+++ Week2/001.Moon/src/Moon.java
@@ -3,6 +3,7 @@ import java.awt.geom.*;
 
 import javafx.application.Application;
 
+import static java.awt.geom.Arc2D.OPEN;
 import static javafx.application.Application.launch;
 
 import javafx.scene.Group;
@@ -33,6 +34,27 @@ public class Moon extends Application {
         graphics.setTransform(new AffineTransform());
         graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+        graphics.translate(this.canvas.getWidth()/2, this.canvas.getHeight()/2);
+//        Arc2D moon = new Arc2D.Double(50,50,100,100,240,110,0);
+
+        GeneralPath moon = new GeneralPath();
+        moon.moveTo(100 * Math.cos(240* Math.PI / 180), -100 * Math.sin(240* Math.PI / 180));
+        for (double i = 241; i <= 460; i++){
+            double rad1 = (i-0.5) * Math.PI / 180;
+            double rad2 = i * Math.PI / 180;
+            double x1 = 100 * Math.cos(rad1);
+            double y1 = -100 * Math.sin(rad1);
+            double x2 = 100 * Math.cos(rad2);
+            double y2 = -100 * Math.sin(rad2);
+            moon.quadTo(x1,y1,x2,y2);
+        }
+
+        moon.moveTo(100 * Math.cos(240* Math.PI / 180), -100 * Math.sin(240* Math.PI / 180));
+        moon.quadTo(50,0,100 * Math.cos(100 * Math.PI / 180),-100 * Math.sin(100 * Math.PI / 180));
+
+        System.out.println(100*Math.cos(240));
+
+        graphics.draw(moon);
     }
 
 
