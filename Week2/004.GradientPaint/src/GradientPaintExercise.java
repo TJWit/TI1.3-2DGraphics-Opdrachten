3da6a424159bc7c8f1b6f6c63379a966d7fe9790
kersjan14 <kersjan14.de.wit@gmail.com>
2023-03-23 19:07:32 +0100

    2.4~

--- Week2/004.GradientPaint/src/GradientPaintExercise.java
+++ Week2/004.GradientPaint/src/GradientPaintExercise.java
@@ -1,10 +1,13 @@
 import java.awt.*;
 import java.awt.geom.*;
-
+import javafx.event.EventHandler;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.input.MouseEvent;
 import javafx.application.Application;
 
 import static javafx.application.Application.launch;
 
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
@@ -14,13 +17,20 @@ import org.jfree.fx.ResizableCanvas;
 
 public class GradientPaintExercise extends Application {
     private ResizableCanvas canvas;
+    private Point2D focus;
 
     @Override
-    public void start(Stage primaryStage) throws Exception
-    {
+    public void start(Stage primaryStage) throws Exception {
         BorderPane mainPane = new BorderPane();
         canvas = new ResizableCanvas(g -> draw(g), mainPane);
         mainPane.setCenter(canvas);
+
+        this.focus = new Point2D.Double(0,0);
+
+        canvas.setOnMouseClicked((MouseEvent e) -> {
+            this.focus = new Point2D.Double(e.getX(), e.getY());
+        });
+
         primaryStage.setScene(new Scene(mainPane));
         primaryStage.setTitle("GradientPaint");
         primaryStage.show();
@@ -28,16 +38,21 @@ public class GradientPaintExercise extends Application {
     }
 
 
-    public void draw(FXGraphics2D graphics)
-    {
+    public void draw(FXGraphics2D graphics) {
         graphics.setTransform(new AffineTransform());
         graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+
+        Area area = new Area(new Rectangle2D.Double(0,0,canvas.getWidth(),canvas.getHeight()));
+
+        float[] fractions = { 0f, 1f };
+        Color[] colors = { Color.RED, Color.BLUE };
+        graphics.setPaint(new RadialGradientPaint(new Point2D.Double(canvas.getWidth()/2, canvas.getHeight()/2), 150f,new Point2D.Double(0,0), fractions, colors, MultipleGradientPaint.CycleMethod.NO_CYCLE));
+        graphics.fill(area);
     }
 
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         launch(GradientPaintExercise.class);
     }
 
