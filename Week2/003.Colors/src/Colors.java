1d112fde587207297613a4aa2c550ddac991a12e
kersjan14 <kersjan14.de.wit@gmail.com>
2023-03-23 18:00:02 +0100

    2.3

--- Week2/003.Colors/src/Colors.java
+++ Week2/003.Colors/src/Colors.java
@@ -1,5 +1,7 @@
 import java.awt.*;
 import java.awt.geom.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import javafx.application.Application;
 
@@ -14,10 +16,25 @@ import org.jfree.fx.ResizableCanvas;
 
 public class Colors extends Application {
     private ResizableCanvas canvas;
+    private ArrayList<Color> colors = new ArrayList<>(
+            Arrays.asList(
+                    Color.black,
+                    Color.blue,
+                    Color.cyan,
+                    Color.darkGray,
+                    Color.gray,
+                    Color.green,
+                    Color.lightGray,
+                    Color.magenta,
+                    Color.orange,
+                    Color.pink,
+                    Color.red,
+                    Color.white,
+                    Color.yellow
+            ));
 
     @Override
-    public void start(Stage primaryStage) throws Exception
-    {
+    public void start(Stage primaryStage) throws Exception {
         BorderPane mainPane = new BorderPane();
         canvas = new ResizableCanvas(g -> draw(g), mainPane);
         mainPane.setCenter(canvas);
@@ -33,6 +50,16 @@ public class Colors extends Application {
         graphics.setTransform(new AffineTransform());
         graphics.setBackground(Color.white);
         graphics.clearRect(0, 0, (int) canvas.getWidth(), (int) canvas.getHeight());
+
+        Area square = new Area(new Rectangle2D.Double(0,0,64,64));
+
+        for (int i = 0; i < 13; i++) {
+            graphics.setColor(Color.black);
+            graphics.draw(square);
+            graphics.setColor(colors.get(i));
+            graphics.fill(square);
+            graphics.translate(64, 0);
+        }
     }
 
 
